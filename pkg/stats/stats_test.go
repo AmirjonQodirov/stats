992c0d431320297e67ce075eb2dc082e25d3357c
AmirjonQodirov <amirqodirov8383@gmail.com>
2021-04-03 00:40:48 +0500

    v2.4.0

--- pkg/stats/stats_test.go
+++ pkg/stats/stats_test.go
@@ -1,23 +1,24 @@
 package stats
 
-import(
-	"testing"
+import (
 	"reflect"
+	"testing"
+
 	"github.com/AmirjonQodirov/bank/v2/pkg/types"
 )
 
 func TestCategoriesAvg_nil(t *testing.T) {
 	var payments []types.Payment
-	result:= CategoriesAvg(payments)
-	if len(result)!=0{
+	result := CategoriesAvg(payments)
+	if len(result) != 0 {
 		t.Error("result len != 0")
 	}
 }
-	
+
 func TestCategoriesAvg_empty(t *testing.T) {
 	payments := []types.Payment{}
-	result:= CategoriesAvg(payments)
-	if len(result)!=0{
+	result := CategoriesAvg(payments)
+	if len(result) != 0 {
 		t.Error("result len != 0")
 	}
 }
@@ -25,37 +26,58 @@ func TestCategoriesAvg_empty(t *testing.T) {
 func TestCategoriesAvg(t *testing.T) {
 	payments := []types.Payment{
 		{
-			ID: 1,
+			ID:       1,
 			Category: "mobile",
-			Amount: 50000,
+			Amount:   50000,
 		},
 		{
-			ID: 2,
+			ID:       2,
 			Category: "mobile",
-			Amount: 50000,
+			Amount:   50000,
 		},
 		{
-			ID: 3,
+			ID:       3,
 			Category: "card",
-			Amount: 10000,
+			Amount:   10000,
 		},
 		{
-			ID: 4,
+			ID:       4,
 			Category: "card",
-			Amount: 10000,
+			Amount:   10000,
 		},
 		{
-			ID: 5,
+			ID:       5,
 			Category: "card",
-			Amount: 10000,
+			Amount:   10000,
 		},
 	}
 	expected := map[types.Category]types.Money{
-		"card":10000,
-		"mobile":50000,
+		"card":   10000,
+		"mobile": 50000,
 	}
-	result:=CategoriesAvg(payments)
-	if !reflect.DeepEqual(expected,result){
-		t.Errorf("invalid result, expected: %v, actual: %v",expected,result)
+	result := CategoriesAvg(payments)
+	if !reflect.DeepEqual(expected, result) {
+		t.Errorf("invalid result, expected: %v, actual: %v", expected, result)
+	}
+}
+
+func TestPeriodsDynamic(t *testing.T) {
+	first := map[types.Category]types.Money{
+		"auto":   10,
+		"food":   20,
+		"mobile": 5,
+	}
+	second := map[types.Category]types.Money{
+		"auto": 5,
+		"food": 3,
+	}
+	expected := map[types.Category]types.Money{
+		"auto":   -5,
+		"food":   -17,
+		"mobile": -5,
+	}
+	result := PeriodsDynamic(first, second)
+	if !reflect.DeepEqual(expected, result) {
+		t.Errorf("invalid result, expected: %v, actual: %v", expected, result)
 	}
 }
